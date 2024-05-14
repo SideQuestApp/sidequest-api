7b07184410d0f97b6c5a55a9801e76852cb953f7
makibrte <milicev2@msu.edu>
2024-05-14 16:13:51 -0400

    added test endpoint and enpoint protection

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from .models import User
 from .serializers import UserRegistrationSerializer
+from oauth2_provider.views.generic import ProtectedResourceView
+from django.http import HttpResponse
 
 
 class RegisterView(generics.CreateAPIView):
@@ -11,3 +13,8 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny, )
     serializer_class = UserRegistrationSerializer
+
+
+class TestApiEndpoint(ProtectedResourceView):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello, OAuth2!')
--- sidequest/urls.py
+++ sidequest/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', view=views.RegisterView.as_view(), name='register'),
     path('api-auth/', include('rest_framework.urls')),  # ! For testing purposes
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider'))
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('api/hello', views.TestApiEndpoint.as_view()),
 ]
