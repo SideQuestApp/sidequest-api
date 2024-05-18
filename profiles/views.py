0f223656a628d1628d6e334b98781e2ae54f4052
makibrte <milicev2@msu.edu>
2024-05-17 21:36:15 -0400

    working on reset password

--- profiles/views.py
+++ profiles/views.py
@@ -3,7 +3,9 @@ from rest_framework.permissions import AllowAny
 from .models import User
 from .serializers import UserRegistrationSerializer
 from oauth2_provider.views.generic import ProtectedResourceView
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from twilio.rest import Client
+import os
 
 
 class RegisterView(generics.CreateAPIView):
@@ -18,3 +20,26 @@ class RegisterView(generics.CreateAPIView):
 class TestApiEndpoint(ProtectedResourceView):
     def get(self, request, *args, **kwargs):
         return HttpResponse('Hello, OAuth2!')
+
+
+class ResetPasswordView(generics.CreateAPIView):
+    """
+    * Password reset view. 
+    * Uses Twilio OTP Verify service
+    * Recieves email -> queries for user with email -> extracts phone number ->
+    * calls verify service
+    * After the service is called the user should recieve the phone number
+    * and verify it with another post request
+    """
+    queryset = User.objects.all()
+
+    def get(self, request, *args, **kwargs):
+
+        email = request.query_params.get('email')
+        users_with_email = User.objects.filter(email=email)
+        user = UserRegistrationSerializer(users_with_email, many=True)
+
+        # Twilio client
+        client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
+
+        client.verify.v2.services(os.environ['TWILIO_SERVICE_SID']).verifications.create(to='+1' + user.data[0]['phone_number'], channel='sms')
