9fe4a8ffd3bdd5ed895465db6afadb3469e7171f
makibrte <milicev2@msu.edu>
2024-05-28 22:28:53 -0400

    created RU out of CRUD endpoints for quests

--- quest/views.py
+++ quest/views.py
@@ -33,3 +33,71 @@ class GetFirstQuestNode(generics.ListCreateAPIView):
         node_query = self.queryset.get(pk=queryset.first_node.pk)
         serializer = QuestNodeSerializer(node_query, many=False)
         return Response(serializer.data)
+
+
+class GetNextAvailableNodes(generics.ListCreateAPIView):
+    permission_classes = (AllowAny, )
+    queryset = QuestNode.objects.all()
+    serializer_class = QuestNodeSerializer
+
+    def get_queryset(self):
+        uuid = self.request.query_params.get('node_uuid')
+        return QuestNode.objects.get(pk=uuid)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        node_query = queryset.next
+        serializer = QuestNodeSerializer(node_query, many=True)
+        return Response(serializer.data)
+
+
+class SetNodeStatus(generics.GenericAPIView):
+    permission_classes = (AllowAny, )
+    queryset = QuestNode.objects.all()
+    serializer_class = QuestNodeSerializer
+    status_codes = {
+        "NS" : "NotStarted",
+        "IP" : "In Progress",
+        "F" : "Finished",
+        "IC" : "Incomplete"
+    }
+
+    def get_queryset(self):
+        uuid = self.request.query_params.get('node_uuid')
+        return QuestNode.objects.get(pk=uuid)
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        node_status = self.request.query_params.get('status')
+        if node_status not in self.status_codes:
+            return HttpResponse('Bad status code')
+        queryset.status = node_status
+        queryset.save()
+        serializer = QuestNodeSerializer(queryset, many=False)
+        return Response(serializer.data)
+
+
+class SetQuestTreeStatus(generics.GenericAPIView):
+    permission_classes = (AllowAny, )
+    queryset = QuestTree.objects.all()
+    serializer_class = QuestTreeSerializer
+    status_codes = {
+        "NS" : "NotStarted",
+        "IP" : "In Progress",
+        "F" : "Finished",
+        "IC" : "Incomplete"
+    }
+
+    def get_queryset(self):
+        uuid = self.request.query_params.get('node_uuid')
+        return QuestTree.objects.get(pk=uuid)
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        node_status = self.request.query_params.get('status')
+        if node_status not in self.status_codes:
+            return HttpResponse('Bad status code')
+        queryset.status = node_status
+        queryset.save()
+        serializer = QuestTreeSerializer(queryset, many=False)
+        return Response(serializer.data)
